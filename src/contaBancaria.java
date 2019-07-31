6991714992c7c3a9189eb459572074e75ccee6b2
Thiago H.R. Costa <thiago_henrique86@hotmail.com>
2019-07-31 18:51:52 -0300

    Criação dos atributos da classe

--- src/contaBancaria.java
+++ src/contaBancaria.java
@@ -1,4 +1,38 @@
 
 public class contaBancaria {
+	
+	private int conta;
+	private double valorDeposito;
+	public String nomeTitular;
+	
+	
+	public contaBancaria(int conta, double valorDeposito, String nomeTitular) {
+		super();
+		this.conta = conta;
+		this.valorDeposito = valorDeposito;
+		this.nomeTitular = nomeTitular;
+	}
+
+
+	public int getConta() {
+		return conta;
+	}
+
+
+	public void setConta(int conta) {
+		this.conta = conta;
+	}
+
+
+	public double getValorDeposito() {
+		return valorDeposito;
+	}
+
+
+	public void setValorDeposito(double valorDeposito) {
+		this.valorDeposito = valorDeposito;
+	}
+	
+	
 
 }
